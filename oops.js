6f2692bbfcf2b597e3fbc8d26c7c6619d979c71b
boytchev <70705048+boytchev@users.noreply.github.com>
2023-10-25 21:43:42 +0300

    Early prototype of default shaders

--- oops.js
+++ oops.js
@@ -1,13 +1,42 @@
 ﻿import * as THREE from 'three';
 
 
+const DEFAULT_VERTEX_SHADER = /* glsl */`
+		varying vec2 vUv;
+
+		void main() {
+			vUv = uv;
+			gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+		}
+		`;
+
+
+const DEFAULT_FRAGMENT_SHADER_HEADER = /* glsl */`
+		uniform sampler2D tDiffuse;
+		varying vec2 vUv;
+
+		vec4 OOPS_PASS_1( vec2 vUv )
+		{
+			return texture2D( tDiffuse, vUv );
+		}
+		`;
+
+
+const DEFAULT_FRAGMENT_SHADER_TAIL = (n) => /* glsl */`
+		void main() {
+			gl_FragColor = OOPS_PASS_${n}( vUv );
+		}
+		`;
+
+
+/*
 const RGBShiftShader = {
 	name: 'oops_rgb_shift',
 	uniforms: {
 		'amount': { value: 0.005, type: 'float' },
 		'angle': { value: 0.0, type: 'float' },
 	},
-	fragmentShader: /* glsl */`
+	fragmentShader: /* glsl * /`
 		vec4 oops_rgb_shift( vUv )
 		{
 			vec2 offset = amount * vec2( cos(angle), sin(angle));
@@ -17,7 +46,7 @@ const RGBShiftShader = {
 			return vec4(cr.r, cga.g, cb.b, cga.a);
 		}`
 };
-
+*/
 
 class OOPSShader
 {
@@ -25,11 +54,10 @@ class OOPSShader
 	{
 		this.name = 'OnlyOnePassShader';
 
-		this.uniforms = {
-			'tDiffuse': { value: null, type: 'sampler2D' },
-		};
+		this.updateShaders( );
+/*		
 
-		this.vertexShader = /* glsl */`
+		this.vertexShader = /* glsl * /`
 			varying vec2 vUv;
 
 			void main() {
@@ -37,24 +65,24 @@ class OOPSShader
 				gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 			}`;
 
-		this.fragmentShader = /* glsl */`
+		this.fragmentShader = /* glsl * /`
 			uniform sampler2D tDiffuse;
 			varying vec2 vUv;
 
 //-----------------------
-			#define dot_screen_center vec2(0.5, 0.5)
-			#define dot_screen_angle 1.57
-			#define dot_screen_scale 4.0
-			#define dot_screen_tSize vec2( 256.0, 256.0 )
-
-			#define oops_level_1 oops_initial
+			#define oops_pass_1 oops_initial
 			vec4 oops_initial( vec2 vUv )
 			{
 				return texture2D( tDiffuse, vUv );
 			}
 			
 //-----------------------
-			#define oops_level_2 oops_dot_screen
+			#define dot_screen_center vec2(0.5, 0.5)
+			#define dot_screen_angle 1.57
+			#define dot_screen_scale 4.0
+			#define dot_screen_tSize vec2( 256.0, 256.0 )
+
+			#define oops_pass_2 oops_dot_screen
 			float dot_screen_pattern( vec2 vUv )
 			{
 				float s = sin( dot_screen_angle ), c = cos( dot_screen_angle );
@@ -66,7 +94,7 @@ class OOPSShader
 			vec4 oops_dot_screen( vec2 vUv )
 			{
 
-				vec4 color = oops_level_1( vUv );
+				vec4 color = oops_pass_1( vUv );
 				float average = ( color.r + color.g + color.b ) / 3.0;
 				return vec4( vec3( average * 10.0 - 5.0 + dot_screen_pattern(vUv) ), color.a );
 			}
@@ -76,22 +104,54 @@ class OOPSShader
 			#define rgb_shift_angle 0.0
 			
 		
-			#define oops_level_3 oops_rgb_shift
+			#define oops_pass_3 oops_rgb_shift
 			vec4 oops_rgb_shift( vec2 vUv )
 			{
 				vec2 offset = rgb_shift_amount * vec2( cos(rgb_shift_angle), sin(rgb_shift_angle));
-				vec4 cr = oops_level_2(vUv + offset);
-				vec4 cga = oops_level_2(vUv);
-				vec4 cb = oops_level_2(vUv - offset);
+				vec4 cr = oops_pass_2(vUv + offset);
+				vec4 cga = oops_pass_2(vUv);
+				vec4 cb = oops_pass_2(vUv - offset);
 				return vec4(cr.r, cga.g, cb.b, cga.a);
 			}
 
 //-----------------------
 			void main() {
-				gl_FragColor = oops_level_3( vUv );
+				gl_FragColor = oops_pass_3( vUv );
 			}`;
-		
+		*/
 	} // OOPSShader.constructor
+	
+	
+	updateShaders( )
+	{
+		this.updateUniforms( );
+		this.updateVertexShader( );
+		this.updateFragmentShader( );
+	} // OOPSShader.updateShaders
+
+	
+	updateUniforms( )
+	{
+		
+		// default uniform (always exists)
+		this.uniforms = {
+			'tDiffuse': { value: null, type: 'sampler2D' },
+		};
+		
+	} // OOPSShader.updateUniforms
+
+	
+	updateVertexShader( )
+	{
+		this.vertexShader = DEFAULT_VERTEX_SHADER;
+	} // OOPSShader.updateVertexShader
+
+	
+	updateFragmentShader( )
+	{
+		this.fragmentShader = DEFAULT_FRAGMENT_SHADER_HEADER;
+		this.fragmentShader += DEFAULT_FRAGMENT_SHADER_TAIL(1);
+	} // OOPSShader.updateFragmentShader
 	
 } // OOPSShader
 			
