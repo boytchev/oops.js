766d11400e47435855411f40591bdc1f9a578cc5
boytchev <70705048+boytchev@users.noreply.github.com>
2023-10-27 19:03:46 +0300

    Finalized (hopefully) shader code style

--- oops.js
+++ oops.js
@@ -7,78 +7,129 @@ const DEFAULT_VERTEX_SHADER = /* glsl */`
 		void main() {
 			vUv = uv;
 			gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-		}
-		`;
+		}`;
+		
+
+const DEFAULT_FRAGMENT_SHADER = {
+	uniforms: {
+	},
+	fragmentShader: /* glsl */`
+		void main() {
+			gl_FragColor = $$( vUv );
+		}`
+};
 
 
-const DEFAULT_FRAGMENT_SHADER_HEADER = /* glsl */`
+
+
+
+const HeaderShader = {
+	uniforms: {
+	},
+	fragmentShader: /* glsl */`
 		uniform sampler2D tDiffuse;
 		varying vec2 vUv;
 
-		vec4 OOPS_PASS_0( vec2 vUv )
+		vec4 $( vec2 vUv )
 		{
 			return texture2D( tDiffuse, vUv );
-		}
-		`;
+		}`
+};
 
-
-const DEFAULT_FRAGMENT_SHADER_TAIL = (n) => /* glsl */`
-		void main() {
-			gl_FragColor = OOPS_PASS_${n}( vUv );
-		}
-		`;
 
 
 const RGBShiftShader = {
-	//name: 'oops_rgb_shift',
 	uniforms: {
-		'amount': { value: 0.005, type: 'float' },
-		'angle': { value: 0.0, type: 'float' },
+		'amount': { value: 0.005 },
+		'angle':  { value: 0.0 }
 	},
-	fragmentShader: (n) => /* glsl */`
-		#define amount_${n+1} 0.005
-		#define angle_${n+1} 0.0
-		#define OOPS_PASS_${n+1} rgb_shift_${n+1}
-		vec4 rgb_shift_${n+1}( vec2 vUv )
+	fragmentShader: /* glsl */`
+		vec4 $( vec2 vUv )
 		{
-			vec2 offset = amount_${n+1} * vec2( cos(angle_${n+1}), sin(angle_${n+1}));
-			vec4 cr = OOPS_PASS_${n}( vUv + offset );
-			vec4 cga = OOPS_PASS_${n}( vUv);
-			vec4 cb = OOPS_PASS_${n}( vUv - offset );
+			vec2 offset = amount_$ * vec2( cos(angle_$), sin(angle_$));
+			vec4 cr  = $$( vUv + offset );
+			vec4 cga = $$( vUv);
+			vec4 cb  = $$( vUv - offset );
 			return vec4( cr.r, cga.g, cb.b, cga.a );
 		}`
 };
 
 
 const DotScreenShader = {
-	//name: 'oops_rgb_shift',
 	uniforms: {
+		'tSize':  { value: new THREE.Vector2( 256, 256 ) },
+		'center': { value: new THREE.Vector2( 0.5, 0.5 ) },
+		'angle':  { value: 1.57 },
+		'scale':  { value: 1.0 }
 	},
-	fragmentShader: (n) => /* glsl */`
-		#define center_${n+1} vec2(0.5, 0.5)
-		#define angle_${n+1} 1.57
-		#define scale_${n+1} 4.0
-		#define tSize_${n+1} vec2( 256.0, 256.0 )
-
-		#define OOPS_PASS_${n+1} dot_screen_${n+1}
-		float pattern_${n+1}( vec2 vUv )
+	fragmentShader: /* glsl */`
+		float pattern_$( vec2 vUv )
 		{
-			float s = sin( angle_${n+1} ), c = cos( angle_${n+1} );
-			vec2 tex = vUv * tSize_${n+1} - center_${n+1};
-			vec2 point = vec2( c * tex.x - s * tex.y, s * tex.x + c * tex.y ) * scale_${n+1};
+			float s = sin( angle_$ ), c = cos( angle_$ );
+			vec2 tex = vUv * tSize_$ - center_$;
+			vec2 point = vec2( c * tex.x - s * tex.y, s * tex.x + c * tex.y ) * scale_$;
 			return ( sin( point.x ) * sin( point.y ) ) * 4.0;
 		}
 
-		vec4 dot_screen_${n+1}( vec2 vUv )
+		vec4 $( vec2 vUv )
 		{
 
-			vec4 color = OOPS_PASS_${n}( vUv );
+			vec4 color = $$( vUv );
 			float average = ( color.r + color.g + color.b ) / 3.0;
-			return vec4( vec3( average * 10.0 - 5.0 + pattern_${n+1}(vUv) ), color.a );
+			return vec4( vec3( average * 10.0 - 5.0 + pattern_$(vUv) ), color.a );
 		}`
 };
 
+
+
+function processUniform( name, value, n )
+{
+	var value = value.value;
+	
+	if( value instanceof THREE.Vector2 )
+		return `#define ${name}_${n+1} vec2(${value.x},${value.y})\n`;
+		
+	if( Number.isInteger(value) )
+		return `#define ${name}_${n+1} ${value.toFixed(1)}\n`;
+		
+	return `#define ${name}_${n+1} ${value}\n`;
+}
+
+
+function processShader( shader, n )
+{
+	var glsl = shader.fragmentShader;
+	
+	// process shader code
+	
+	// $$ -> main_|n|
+	glsl = glsl.replaceAll( '$$', `main_${n}` );
+	
+	// _$ -> _|n+1|
+	glsl = glsl.replaceAll( '_$', `_${n+1}` );
+	
+	// $( -> main_|n+1|
+	glsl = glsl.replaceAll( '$', `main_${n+1}` );
+
+	glsl += `\n`;
+
+	// process uniforms
+	if( shader.uniforms )
+	for( var name of Object.keys(shader.uniforms) )
+	{
+		glsl = processUniform( name, shader.uniforms[name], n ) + glsl;
+	}
+	
+	glsl += `\n`;
+	
+	return glsl;
+}
+
+
+
+
 const SHADERS = {
+		HeaderShader: HeaderShader,
 		RGBShiftShader: RGBShiftShader,
 		DotScreenShader: DotScreenShader,
 }
@@ -90,12 +141,9 @@ class OOPSShader
 	{
 		this.name = 'OnlyOnePassShader';
 		this.passes = [];
+		this.addShader( 'HeaderShader' );
 		
 		this.updateShaders( );
-/*		
-
-
-		*/
 	} // OOPSShader.constructor
 	
 	
@@ -126,14 +174,17 @@ class OOPSShader
 	
 	updateFragmentShader( )
 	{
-		this.fragmentShader = DEFAULT_FRAGMENT_SHADER_HEADER;
+		this.fragmentShader = '';
 		
 		for( var i=0; i<this.passes.length; i++ )
 		{
-			this.fragmentShader += this.passes[i].fragmentShader( i );
+			this.fragmentShader += processShader( this.passes[i], i );
 		}
 			
-		this.fragmentShader += DEFAULT_FRAGMENT_SHADER_TAIL( this.passes.length );
+		this.fragmentShader += processShader( DEFAULT_FRAGMENT_SHADER, i );
+		
+		console.log('----------------------------');
+		console.log(this.fragmentShader);
 		
 	} // OOPSShader.updateFragmentShader
 
